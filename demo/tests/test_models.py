4f339d9d10ac1e7a42d0da35d671063eeb44139b
Simon Fontaine <github@simonfontaine.com>
2025-11-12 16:11:56 +0100

    [FIX] move create from assertRaises

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -24,11 +24,11 @@ class TestUsers(TransactionCase):
 
     def test_description_is_required(self):
         with mute_logger('odoo.sql_db'):
-            with self.assertRaises(Exception):
-                user = self.env['res.users'].create({
+            user = self.env['res.users'].create({
                     'name': 'Marie-Noël',
                     'login': 'mnv',
                 })
+            with self.assertRaises(Exception):
                 user.description = None
 
 
